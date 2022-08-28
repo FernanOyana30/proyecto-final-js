45d733973eee3de028b90260b4843667d1a03451
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2022-08-28 02:35:49 -0300

    comentarios

--- script.js
+++ script.js
@@ -8,49 +8,16 @@ class Producto {
     }
 }
 
-const producto1 = new Producto ("Tubo de calamar", "Tubo de calamar para rabas. Precio xkg", 1200, 30)
-const producto2 = new Producto ("Cornalitos", "Cornalitos congelados. Precio xkg", 500, 50)
-
-const producto3 = {... producto1}
-producto3.nombre = "Calamar entero"
-producto3.precio = 700
-producto3.descripcion = "Calamar entero con grasa, tentáculos y timón. Ideal para cazuela. Precio xkg"
-
-const producto4 = {... producto1}
-producto4.nombre = "Rabas congeladas"
-producto4.descripcion = "Rabas precocidas y rebozadas listas para freír. Precio xkg"
-
-
-const TIENDA_ARRAY = [producto1, producto2, producto3, producto4]
-
-const contenedorTienda = document.getElementById("contenedorTienda")
-
-
-
-TIENDA_ARRAY.forEach(producto => {
-    contenedorTienda.innerHTML += `    
-        <div class="card" id="producto${producto.indice}" style="width: 18rem;margin:3px;">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>   
-                <p class="card-text"> ${producto.descripcion}</p>                 
-                <p class="card-text"> $${producto.precio}</p>
-                <p class="card-text">${producto.stock} disponibles</p>   
-                <button class="btn btn-danger">Añadir al carrito</button>                 
-            </div>
-        </div>  
-    `
-})
-
-//
-
+//Array de productos vacío
 let productos = []
 
 if(localStorage.getItem('productos')) { 
-    productos = JSON.parse(localStorage.getItem('productos')) 
+    productos = JSON.parse(localStorage.getItem('productos')) //Pasar de JSON a objeto
 } else {
-    localStorage.setItem('productos', JSON.stringify(productos)) 
+    localStorage.setItem('productos', JSON.stringify(productos)) //Pasar de objeto a JSON
 }
 
+//Interacción con HTML
 const form = document.getElementById("form")
 const mostrarProductos = document.getElementById("mostrarProductos")
 const divProductos = document.getElementById("divProductos")
@@ -62,12 +29,12 @@ form.addEventListener('submit', (event) => {
     const precio = document.getElementById("precioProducto").value
     const stock = document.getElementById("stockProducto").value  
     const mensajeAlerta = document.getElementById("mensajeAlerta") 
-        if(precio < 0 || stock < 0 ){
+        if(precio < 0 || stock < 0 ){ //Si precio o stock son negativos, se cancela la creación del producto
             Swal.fire('Introduzca un valor de precio/stock igual o mayor que 0')                                  
             return            
         } 
         
-        else if(precio >= 0 || stock >= 0 ){            
+        else if(precio >= 0 || stock >= 0 ){ //Si precio o stock son iguales y mayores a 0, el producto es creado.        
             const producto = new Producto (nombre, descripcion, precio, stock)
             productos.push(producto)    
             localStorage.setItem("productos", JSON.stringify(productos))
@@ -83,6 +50,7 @@ form.addEventListener('submit', (event) => {
         }                 
 })
 
+//Evento para mostrar productos creados
 mostrarProductos.addEventListener('click', () => {
     const prodStorage = JSON.parse(localStorage.getItem('productos')) 
     divProductos.innerHTML = ""
@@ -99,10 +67,10 @@ mostrarProductos.addEventListener('click', () => {
             </div>        
         `        
     })
-
+    
     prodStorage.forEach((producto, indice) => {
         const tarjetaProducto = document.getElementById(`producto${indice}`)        
-        tarjetaProducto.children[0].children[4].addEventListener('click', () => {            
+        tarjetaProducto.children[0].children[4].addEventListener('click', () => { //Eliminar productos guardados            
               const swalWithBootstrapButtons = Swal.mixin({
                 customClass: {
                   confirmButton: 'btn btn-success',
@@ -120,10 +88,10 @@ mostrarProductos.addEventListener('click', () => {
                 reverseButtons: true
               }).then((result) => {
                 if (result.isConfirmed) {
-                    tarjetaProducto.remove()
-                    productos.splice(indice, 1)
-                    localStorage.setItem('productos', JSON.stringify(productos))    
-                    //console.log(`${producto.nombre} eliminado`)
+                    tarjetaProducto.remove()//Eliminar del DOM
+                    productos.splice(indice, 1)//Eliminar del Array
+                    localStorage.setItem('productos', JSON.stringify(productos))//Eliminar del Local Storage    
+                    console.log(`${producto.nombre} eliminado`)
                   swalWithBootstrapButtons.fire(                
                     'Producto eliminado',
                     'Tu producto ha sido eliminado con éxito.'
