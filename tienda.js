ee9c9b78a9373aa2126f318da598c4e21a002ae6
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2022-09-03 23:06:20 -0300

    imagenes de productos

--- tienda.js
+++ tienda.js
@@ -1,6 +1,6 @@
 class Producto {
-    constructor(nombre, descripcion, precio, stock) {        
-        
+    constructor(img, nombre, descripcion, precio, stock) {        
+        this.img = img
         this.nombre = nombre
         this.descripcion = descripcion        
         this.precio = parseFloat(precio)
@@ -8,15 +8,17 @@ class Producto {
     }
 }
 
-const producto1 = new Producto ("Tubo de calamar", "Tubo de calamar para rabas. Precio xkg", 1200, 30)
-const producto2 = new Producto ("Cornalitos", "Cornalitos congelados. Precio xkg", 500, 50)
+const producto1 = new Producto ("../img/tubo-calamar.jpg", "Tubo de calamar", "Tubo de calamar para rabas. Precio xkg", 1200, 30)
+const producto2 = new Producto ("../img/cornalitos-congelados.jpg", "Cornalitos", "Cornalitos congelados. Precio xkg", 500, 50)
 
 const producto3 = {... producto1}
+producto3.img = "../img/calamar-entero.jpg"
 producto3.nombre = "Calamar entero"
 producto3.precio = 700
 producto3.descripcion = "Calamar entero con grasa, tentáculos y timón. Ideal para cazuela. Precio xkg"
 
 const producto4 = {... producto1}
+producto4.img = "../img/rabas-precocidas.jpg"
 producto4.nombre = "Rabas congeladas"
 producto4.descripcion = "Rabas precocidas y rebozadas listas para freír. Precio xkg"
 
@@ -25,11 +27,10 @@ const TIENDA_ARRAY = [producto1, producto2, producto3, producto4]
 
 const contenedorTienda = document.getElementById("contenedorTienda")
 
-
-
 TIENDA_ARRAY.forEach(producto => {
     contenedorTienda.innerHTML += `    
         <div class="card" id="producto${producto.indice}" style="width: 18rem;margin:3px;">
+            <img src="./img/${producto.img}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${producto.nombre}</h5>   
                 <p class="card-text"> ${producto.descripcion}</p>                 
@@ -50,6 +51,7 @@ fetch('./json/productos.json')
         contenedorTienda2.innerHTML += 
         `
         <div class="card" id="product${indice}" style="width: 18rem;margin:3px;">
+            <img src="./img/${producto.img}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${producto.nombre}</h5>   
                 <p class="card-text"> ${producto.descripcion}</p>                 
